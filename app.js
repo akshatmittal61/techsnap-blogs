2eda59defacdd470f86c5082e71d60d162b7673f
akshatmittal61 <akshatmittal2506@gmail.com>
2022-01-12 00:46:16 +0530

    added scroll to top

--- app.js
+++ app.js
@@ -171,3 +171,15 @@ blogs.forEach((blog) => {
 	Blog.appendChild(blogContainer);
 	blogsLayout.appendChild(Blog);
 });
+const fab = document.querySelector(".scroll-to-top");
+const header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+	fab.style.display = window.scrollY > window.innerHeight ? "flex" : "none";
+	header.style.position =
+		window.scrollY > window.innerHeight ? "fixed" : "relative";
+	header.style.boxShadow =
+		window.scrollY > window.innerHeight ? "var(--shadow-3d)" : "none";
+});
+fab.addEventListener("click", () => {
+	window.scrollTo(0, 0);
+});
